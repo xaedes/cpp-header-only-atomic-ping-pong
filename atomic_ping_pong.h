b35f5df576b6b05e42e6c06ff1ba60e6f1dbb25e
xaedes <xaedes@googlemail.com>
2020-09-24 16:19:43 +0200

    Create atomic_ping_pong.h

---
+++ atomic_ping_pong.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <atomic>
+
+template <typename T>
+class AtomicPingPong
+{
+public:
+    T items[2];
+    std::atomic<int> current = 0; // only least significant bit is used
+    AtomicPingPong<T>& swap();
+
+    T& operator()();
+    const T& operator()() const;
+
+    T& get();
+    const T& get() const;
+
+    T& other();
+    const T& other() const;
+};
+
+// implementation:
+template <typename T>
+AtomicPingPong<T>& AtomicPingPong<T>::swap()
+{
+    // xor with 1 will toggle between 0 and 1
+    current ^= 1;
+    return *this;
+}
+
+template <typename T>
+T& AtomicPingPong<T>::operator()()
+{
+    return items[current & 1];
+}
+
+template <typename T>
+const T& AtomicPingPong<T>::operator()() const
+{
+    return items[current & 1];
+}
+
+template <typename T>
+T& AtomicPingPong<T>::get()
+{
+    return items[current & 1];
+}
+
+template <typename T>
+const T& AtomicPingPong<T>::get() const
+{
+    return items[current & 1];
+}
+
+template <typename T>
+T& AtomicPingPong<T>::other()
+{
+    return items[1-(current & 1)];
+}
+
+template <typename T>
+const T& AtomicPingPong<T>::other() const
+{
+    return items[1-(current & 1)];
+}
